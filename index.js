539bde53e969811ea74bc2e776ffffa6a0176700
K-S-Harish-Balagi <85448207+K-S-Harish-Balagi@users.noreply.github.com>
2021-06-06 20:58:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let inputField = document.getElementById('input-field');
+let hours = document.getElementById('hours');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let totalTime, handle;
+
+function reset() {
+    hours.innerText = '0';
+    minutes.innerText = '0';
+    seconds.innerText = '0';
+    inputField.value = '';
+    clearInterval(handle);
+    document.getElementsById('start').disabled = false
+}
+
+function startTimer() {
+    totalTime = parseInt(inputField.value);
+    handle = setInterval(timer, 1000);
+    document.getElementsById('start').disabled = true
+}
+
+function timer() {
+    formatTime(totalTime);
+    if (totalTime <= 0) reset();
+    else totalTime--;
+}
+
+function formatTime(second) {
+    const hh =parseInt(second/3600);
+    second = second%3600;
+    const mm =parseInt(second/60);
+    second %= 60;
+    hours.innerText = hh;
+    minutes.innerText = mm;
+    seconds.innerText = second; 
+}
+
